5d1285fffdef3021c95fb99b39b723f579e3917b
Guillaume Guirado <30233944+g-guirado@users.noreply.github.com>
2020-03-23 13:26:15 +0100

    Show stacktrace in case the flag hasn't been registered before being used

--- src/flag.ts
+++ src/flag.ts
@@ -2,6 +2,15 @@ import { Flag as _Flag, Rox } from 'rox-ssr';
 
 export class Flag extends _Flag {
   isEnabled(context?: any): boolean {
+    try {
+      if (!this.name) {
+        throw new Error('Flag value requested on a flag that wasn\'t registered. Did you initialize Rollout properly?');
+      }
+    } catch (e) {
+      console.warn(e); // eslint-disable-line
+
+      return this.defaultValue === 'true';
+    }
     return Rox.dynamicApi.isEnabled(this.name, this.defaultValue === 'true', context);
   }
 }
